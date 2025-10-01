a5812b37a010966d75bb836f6a1a8dcbd30a232d
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-01 22:34:46 +0600

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,25 @@
+/*Print the patterns
+  *****
+  *****
+  *****
+  *****
+  */
+
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }      
+    }
+}
+
+
