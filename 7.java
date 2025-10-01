6311115fffce542fc2d37a3a58f473de016bea1a
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-01 23:03:43 +0600

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,23 @@
+/*Print the patterns
+12345
+1234
+123
+12
+1
+  */
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 5; i >= 1; i--)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }      
+    }
+}
