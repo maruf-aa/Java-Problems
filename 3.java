c3e5dad4a32ef0c8be63dd41e163e94a96fd5782
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-01 22:41:28 +0600

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,1 +1,23 @@
+/*Print the patterns
+  *
+  **
+  ***
+  ****
+  */
 
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }      
+    }
+}
