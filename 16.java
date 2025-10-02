ef5a9233e3a8c450acc1428f7a491e5b80ebc9ce
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-02 23:03:49 +0600

    Update 16.java

--- 16.java
+++ 16.java
@@ -6,7 +6,12 @@ public class Firstclass {
     {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for (int i = 2; i < a; i++) 
+        if(a == 1)
+        {
+            System.out.println("Nither prime number not composite");
+            return;
+        }
+        for (int i = 2; i <= a / 2; i++) 
         {
             if (a % i == 0) 
             {
