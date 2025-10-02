1307a3b0c1af774cd04efdb49d9af0b86e2ee852
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-02 13:35:52 +0600

    Add star pattern printing program in 10.java
    Implement a Java program to print a star pattern.

---
+++ 10.java
@@ -0,0 +1,28 @@
+/*Print the patterns
+    *****
+   *****
+  *****
+ *****
+***** 
+  */
+
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5 - i; j++) 
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 5; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }      
+    }
+}
