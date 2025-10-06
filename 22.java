fca9c46015a451b99a1d79a5368ee7d5d31dd747
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-06 11:56:26 +0600

    Add inverted half pyramid printing
    Implement inverted half pyramid printing based on user input.

---
+++ 22.java
@@ -0,0 +1,25 @@
+/* Print Inverted Half Pyramid.
+1111
+222
+33
+4
+  */
+
+import java.util.*;
+public class Firstclass {
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= (n - i + 1); j++) 
+            {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}
