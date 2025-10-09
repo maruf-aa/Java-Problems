9fb0f101163478374988dbd9b8bf70a0cdcd9aa1
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-09 11:00:10 +0600

    Add multiply function and main method in Firstclass

---
+++ 23.java
@@ -0,0 +1,17 @@
+ //Write a function to multiply 2 numbers.
+
+import java.util.*;
+public class Firstclass {
+    
+    public static int multiply(int a,int b)
+    {
+        return a * b;
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(multiply(a, b));
+    }
+}
